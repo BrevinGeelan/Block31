0de98fd37b73cf9681f4906b43e4edf50c198a44
Brevin Geelan <yevin22@gmail.com>
2023-08-14 08:16:20 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+// import the pets array from data.js
+const pets = require('./data');
+
+// init express app
+const express = require('express');
+const app = express();
+const path= require('path');
+
+const PORT = 8080;
+
+// GET - / - returns homepage
+app.get('/', (req, res) => {
+    // serve up the public folder as static index.html file
+res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// hello world route
+app.get('/api', (req, res) => {
+    res.send('Hello World!');
+});
+
+// get all pets from the database
+app.get('/api/v1/pets', (req, res) => {
+    // send the pets array as a response
+res.json(pets);
+});
+
+// get pet by owner with query string
+app.get('/api/v1/pets/owner', (req, res) => {
+    // get the owner from the request
+const owner = req.query.owner; // declare owner
+
+    // find the pet in the pets array
+    const OwnerPet = pets.find(pet => pet.owner === owner);
+
+    // send the pet as a response
+if (OwnerPet) {
+    res.json(OwnerPet);
+} else {
+    res.status(404).json({message: 'No pet found for this owner..'}); //error
+}
+});
+
+// get pet by name
+app.get('/api/v1/pets/:name', (req, res) => {
+    // get the name from the request
+const name = req.params.name; //use params for pet name.
+
+    // find the pet in the pets array
+    const pet = pets.find(pet => pet.name === name);
+
+    // send the pet as a response
+    if (pet) {
+        res.json(pet);
+    } else {
+        res.status(404).json({message: 'no pet found with this name'}) //send error if no pet is found
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port http://localhost:${PORT}`);
+});
+
+module.exports = app;
